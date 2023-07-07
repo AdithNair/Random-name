e83d63419f546f1ac1c2302ba0d8deed7edf7a60
AdithNair <104628360+AdithNair@users.noreply.github.com>
2023-07-07 11:19:31 -0400

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,12 @@
+list = ["Soccer", "Basketball", "Tennis", "Golf", "Swim"]
+
+print(list)
+
+list.append("Cricket")
+
+print(list)
+
+
+list2 = ["Adidas", "Nike", "Puma", "Under Armor"]
+
+print(len(list2))
